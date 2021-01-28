014e2d252512c868f46ec30fe9bce3b35066dc9f
PipZip <37911683+PipZip@users.noreply.github.com>
2021-01-28 17:08:54 +0600

    php/fav

--- src/php/post.php
+++ src/php/post.php
@@ -1,22 +1,43 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
 
 if(isset($_POST["name"]) && isset($_POST["phone"]) && isset($_POST["email"]) && isset($_POST["text"])) {
-        $to = "agency@mockup.digital";
-    $subject = "Форма обратной связи";
+    $mail = new PHPMailer(true);
 
-    $message = "
-    <p><b>Имя:</b> ".htmlspecialchars(trim($_POST["phone"]))."</p>
-    <p><b>Телефон:</b> ".htmlspecialchars(trim($_POST["tel"]))."</p>
-    <p><b>Email:</b> ".htmlspecialchars(trim($_POST["email"]))."</p>
-    <p><b>Тема:</b> ".htmlspecialchars(trim($_POST["subject"]))."</p>
-    <p style='white-space: pre-line;'><b>Сообщение:</b> ".htmlspecialchars(trim($_POST["text"]))."</p>
-    ";
+    try {
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.yandex.ru';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'agency@mockup.digital';
+        $mail->Password   = '';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = 465;
 
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        //Recipients
+        $mail->setFrom('agency@mockup.digital', 'mockup.digital');
+        $mail->addAddress('agency@mockup.digital');     // Add a recipient
 
-    $headers .= 'From: <agency@mockup.digital>' . "\r\n";
+        // Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Форма обратной связи';
+        $mail->Body    = "
+                        <p><b>Имя:</b> ".htmlspecialchars(trim($_POST["name"]))."</p>
+                        <p><b>Телефон:</b> ".htmlspecialchars(trim($_POST["phone"]))."</p>
+                        <p><b>Email:</b> ".htmlspecialchars(trim($_POST["email"]))."</p>
+                        <p><b>Тема:</b> ".htmlspecialchars(trim($_POST["subject"]))."</p>
+                        <p style='white-space: pre-line;'><b>Сообщение:</b> ".htmlspecialchars(trim($_POST["text"]))."</p>
+                        ";
 
-    mail($to,$subject,$message,$headers);
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
 ?>
